f02d68f6bd47ff688995b7cf782e235cb53b293f
eysteing <eysteing@gmail.com>
2022-01-29 01:35:31 +0100

    Beta 0.4 - Removed a for loop that was not necsecery

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from send2trash import send2trash
 from progress import copyfile
 from plex_update import update_plexapi
 
-logging.basicConfig(filename='log films.log',
+logging.basicConfig(filename='log/log films.log',
                     level=logging.INFO,
                     format='%(asctime)s:%(message)s')
 
@@ -28,6 +28,10 @@ def camel_case(s):
     return ''.join([s[:]])
 
 
+def subtitles():
+    sub_extensions = ["srt", "sub"]
+
+
 while True:
     time.sleep(10)
 
@@ -37,61 +41,61 @@ while True:
 
     if added:
         logging.info(f"Added: {added}")
-        film_lst = []
-        path_lst = []
+        # film_lst = []
+        # path_lst = []
 
         for path, subdirs, files in os.walk(f"{paths}"):
             for name in files:
                 for extension in extensions:
                     if re.findall(extension, name):
-                        film_lst.append(name)
-                        path_lst.append(path + "/" + name)
+                        film_lst = name
+                        path_lst = path + "/" + name
 
-        for name in film_lst:
+        # for name in film_lst:
             # --------TV--------
-            tv = re.findall(r"""
-            (.*)  # Title
-            [ .]
-            S(\d{1,2})  # Season
-            E(\d{1,2})  # Episode
-            [ .a-zA-Z]* # Space, period or words like Proper/buried
-            (\d{3,4}p)? # Quality
-            """, camel_case(name), re.VERBOSE)
+        tv = re.findall(r"""
+        (.*)  # Title
+        [ .]
+        S(\d{1,2})  # Season
+        E(\d{1,2})  # Episode
+        [ .a-zA-Z]* # Space, period or words like Proper/buried
+        (\d{3,4}p)? # Quality
+        """, camel_case(name), re.VERBOSE)
 
-            if tv:
-                p = Path(path_lst[0])
-                try:
-                    path_old_tv = p.rename(Path(p.parent, f"{tv[0][0]}.S{tv[0][1]}E{tv[0][2]}{p.suffix}"))
-                except FileNotFoundError as e:
-                    logging.info(e)
+        if tv:
+            p = Path(path_lst)
+            try:
+                path_old_tv = p.rename(Path(p.parent, f"{tv[0][0]}.S{tv[0][1]}E{tv[0][2]}{p.suffix}"))
+            except FileNotFoundError as e:
+                logging.info(e)
 
-                for path, subdirs, files in os.walk(f"{path_tv}"):
-                    tvs = tv[0][0].replace(".", " ")
-                    tvs = camel_case(tvs)
-                    season = tv[0][1]
+            for path, subdirs, files in os.walk(f"{path_tv}"):
+                tvs = tv[0][0].replace(".", " ")
+                tvs = camel_case(tvs)
+                season = tv[0][1]
 
-                    if season[0] == '0':
-                        season = season.replace("0", "")
-                    if tvs in subdirs:
-                        print(f"{tv[0][0]}.S{tv[0][1]}E{tv[0][2]}")
-                        new_path = path_tv \
-                                   + "/" \
-                                   + tvs \
-                                   + "/" \
-                                   + "Season " + season \
-                                   + "/" \
-                                   + f"{tv[0][0]}.S{tv[0][1]}E{tv[0][2]}" \
-                                     f" - {time.asctime(time.localtime(time.time()))}{p.suffix}"
-                        break
-                try:
-                    copyfile(path_old_tv, new_path)
-                    send2trash(path_old_tv)
-                    logging.info(f"{tvs} was add to: {new_path}")
-                    update_plexapi()
-                    print("\n Done")
-                    logging.info("Done")
-                except NameError as e:
-                    logging.info(e)
+                if season[0] == '0':
+                    season = season.replace("0", "")
+                if tvs in subdirs:
+                    print(f"{tv[0][0]}.S{tv[0][1]}E{tv[0][2]}")
+                    new_path = path_tv \
+                               + "/" \
+                               + tvs \
+                               + "/" \
+                               + "Season " + season \
+                               + "/" \
+                               + f"{tv[0][0]}.S{tv[0][1]}E{tv[0][2]}" \
+                                 f" - {time.asctime(time.localtime(time.time()))}{p.suffix}"
+                    break
+            try:
+                copyfile(path_old_tv, new_path)
+                send2trash(path_old_tv)
+                logging.info(f"{tvs} was add to: {new_path}")
+                update_plexapi()
+                print("\n Done")
+                logging.info("Done")
+            except NameError as e:
+                logging.info(e)
 
             movie = re.findall(r"""
             (.*?[ .]\d{4})  # Title including year
@@ -101,7 +105,7 @@ while True:
 
             # -----------Movie-------------
             if movie:
-                p = Path(path_lst[0])
+                p = Path(path_lst)
                 try:
                     path_old_mov = p.rename(Path(p.parent, f"{movie[0][0]}{p.suffix}"))
                     movs = movie[0][0].replace(".", " ")
--- plex_update.py
+++ plex_update.py
@@ -5,8 +5,4 @@ def update_plexapi():
     baseurl = 'http://192.168.1.9:32400'
     token = 'z2bbh3MJ3pYjJdLcZFDp'
     plex = PlexServer(baseurl, token)
-
-    # ------- Find Clients -------
-    clients = [client.title for client in plex.clients()]
-
     plex.library.update()
